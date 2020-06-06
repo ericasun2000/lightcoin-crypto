24a4b29f9a2ebaac0c7a8aadc159d547aeab098e
Erica Sun <ericasun@seas.upenn.edu>
2020-06-06 17:18:12 +0000

    OOP refactoring using classes

--- index.js
+++ index.js
@@ -1,29 +1,80 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    let result = 0;
+    this.transactions.forEach(t => {
+      result += t.value;
+    });
+    return result;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
+class Transaction {
 
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
 
+  commit() {
+    if (this.isAllowed()) {
+      // Keep track of the time of the transaction
+      this.time = new Date();
+      // Add the transaction to the account
+      this.account.addTransaction(this);
+      return true;
+    }
+    return false;
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return (this.account.balance > this.amount);
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
+  }
+}
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account('Erica');
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Starting Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+const t1 = new Withdrawal(50.00, myAccount);
+console.log('Commit result of withdrawing $50 when balance is $0)', t1.commit());
+
+const t2 = new Deposit(120.00, myAccount);
+console.log('Commit result of depositing $120 when balance is $0', t2.commit());
+
+const t3 = new Withdrawal(20.00, myAccount);
+console.log('Commit result of depositing $20 when balance is $120', t3.commit());
+
+console.log('Ending Balance:', myAccount.balance);
